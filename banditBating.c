a21c8981dbaabaa0e8a4e58628172debe6e6debc
collin vanmeter <metervancollin@gmail.com>
2024-01-18 21:30:02 -0500

    adding some logic for assignment

--- banditBating.c
+++ banditBating.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 #define NAME_SIZE 20
+#define LOCATION_COUNT 3
+#define PLACEMENT_COUNT 5
 
 
 struct locationOutline 
 {
-    char* locationName;
+    char locationName;
     long long int* placementList;
     long long int totalAmountStolen;
 };
@@ -15,20 +17,10 @@ struct locationOutline
 struct placementOutline
 {
     int ID;
+    long long int foodLevel;
     long long int prevFoodLevel;
     long long int individualAmountStolen;
 };
-
-void collectStarterInfo(char *n, long long int *d, long long int *f) //name, ID, food level
-{
-    scanf("%s %lld %lld", n, d, f);
-}
-
-
-
-
-
-
 
 
 int main() 
@@ -36,12 +28,30 @@ int main()
 
     long long int d,f;
     char n[NAME_SIZE];
+    int i = 0;
+    int endToken = -1;
 
 
 
-    collectStarterInfo(n, &d, &f);
+    collectLocationInfo(n, &d, &f);
 
     printf("%s %lld %lld\n", n, d, f);
+
+
+    struct locationOutline locations[30];
+
+    struct placementOutline placements[30];
+
+
+    do
+    {
+        scanf("%s %lld %lld", locations[i].locationName, placements[i].ID, placements[i].foodLevel);
+        i++;
+
+    } while (endToken != placements[i].ID);
+    
+
+
 
     return 0;
 }
