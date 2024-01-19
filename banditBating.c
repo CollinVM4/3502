a05a1a66e5cf85cb502e5b29f915fefb1e403d6a
collin vanmeter <metervancollin@gmail.com>
2024-01-19 17:15:24 -0500

    adding fixes to assignment

--- banditBating.c
+++ banditBating.c
@@ -6,52 +6,42 @@
 #define LOCATION_COUNT 3
 #define PLACEMENT_COUNT 5
 
-
+// struct for locations
 struct locationOutline 
 {
-    char locationName;
-    long long int* placementList;
+    char locationName[NAME_SIZE];
+    long long int placementList;
     long long int totalAmountStolen;
 };
 
+// struct for individual placements 
 struct placementOutline
 {
-    int ID;
+    long long int ID;
     long long int foodLevel;
     long long int prevFoodLevel;
     long long int individualAmountStolen;
 };
 
-
 int main() 
 {
-
-    long long int d,f;
-    char n[NAME_SIZE];
     int i = 0;
     int endToken = -1;
-
-
-
-    collectLocationInfo(n, &d, &f);
-
-    printf("%s %lld %lld\n", n, d, f);
-
+    int numEntries = 0;
 
     struct locationOutline locations[30];
-
     struct placementOutline placements[30];
-
 
     do
     {
-        scanf("%s %lld %lld", locations[i].locationName, placements[i].ID, placements[i].foodLevel);
-        i++;
-
+        scanf("%s %lld %lld", locations[i].locationName, &placements[i].ID, &placements[i].foodLevel);
+        i++; numEntries++;
     } while (endToken != placements[i].ID);
     
-
-
+    for (int i = 0; i < numEntries; i++)
+    {
+        printf("%s\n", locations[i].locationName);
+    }
 
     return 0;
 }
