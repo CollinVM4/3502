f83ad3879ed55d08d12af7bcf92b858908a483b9
collin vanmeter <metervancollin@gmail.com>
2024-01-20 14:00:04 -0500

    adding food checking logic and new entry logic to banditBaiting assignment

--- banditBating.c
+++ banditBating.c
@@ -31,19 +31,74 @@ int main()
 {
     int i = 0; // for loop usage 
     long long int endToken = -1; // setting up endToken for do,while loop
-    int numEntries = 0; // use in for loop, printing out results
+    int numEntries, isNew, checkFood = 0; // use in for loop, printing out results
+
 
     struct locationOutline locations[30]; // instance of array of structs
-
 
 
     // scan for entries, terminate when "END -1 -1" is entered 
     do
     {
-        scanf("%s %lld %lld", locations[i].locationName, &locations[i].placementList[i].ID, &locations[i].placementList[i].foodLevel); // scan for req. info
+        // scan for req. info
+        scanf("%s %lld %lld", locations[i].locationName, &locations[i].placementList[i].ID, 
+        &locations[i].placementList[i].foodLevel);
+
+        // increase indexes
         i++; numEntries++;
+        
+
+        // walk list to determine if entry is new 
+        for (int j = 0; j < numEntries; j++) // loop through all names
+        {
+            if (strcmp(locations[i].locationName, locations[j].locationName) == 0) // 0 is equal, anything else is not equal
+            {
+                // now check if new placement
+                for (int k = 0; k < numEntries; k++) // loop through all placements
+                {
+                    if(locations[i].placementList[i].ID == locations[k].placementList[k].ID) // not new 
+                    {
+                        checkFood = 1; // will run check food block
+                        isNew = 0; // will not add to main struct list 
+                    }
+                }
+            } 
+        };
+        
+        // check food logic
+        if (checkFood == 1) 
+        {
+            // check if new input food level is less than previous food level, if so, FOOD STOLEN!!! 
+            if (locations[i].placementList[i].foodLevel < locations[i].placementList[i].prevFoodLevel)  // if food stolen
+            {
+                locations[i].placementList[i].individualAmountStolen =
+                locations[i].placementList[i].foodLevel - locations[i].placementList[i].prevFoodLevel;
+            } else if (locations[i].placementList[i].foodLevel == locations[i].placementList[i].prevFoodLevel) // if no food stolen or added
+            {
+                locations[i].placementList[i].foodLevel = locations[i].placementList[i].prevFoodLevel;
+            } else // if added food
+            {
+                locations[i].placementList[i].prevFoodLevel = locations[i].placementList[i].foodLevel;
+            } 
+        }
+        
+
+
+
+
     } while (locations[i - 1].placementList[i - 1].ID != endToken); // will be active until a "-1" is scanned into an ID slot
     
+
+
+
+
+
+
+
+
+
+
+
 
 
     // print out the results 
