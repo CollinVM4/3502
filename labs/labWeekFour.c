04fb4106e2421c216263757aa800fd4fd94ab617
collin vanmeter <metervancollin@gmail.com>
2024-02-09 20:25:20 -0500

    a2 note

---
+++ labs/labWeekFour.c
@@ -0,0 +1,27 @@
+/*
+    POSTFIX / INFIX EXPRESSION 
+
+    - Queue
+    - using stack (aka pop, push)
+
+
+
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
