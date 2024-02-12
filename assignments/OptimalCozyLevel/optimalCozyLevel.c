0c717db75b3cdce6324433cd51e0aa6e00600d58
collin vanmeter <metervancollin@gmail.com>
2024-02-12 14:56:35 -0500

    planning out assignment

--- assignments/OptimalCozyLevel/optimalCozyLevel.c
+++ assignments/OptimalCozyLevel/optimalCozyLevel.c
@@ -9,17 +9,45 @@
 
     note: MUST USE RECURSION
 
+    hint: use recursion class examples for help 
 
+    important advice: 
     no structs, only recursive function and main, use global memory to hold arrays and answers, 
     account for negative answers, base case: make sure each hose has a cat assigned
 
 
     input:
+        1st line: num of cats
+        
+        N lines: each cat receives 3 cozy level ints (?) 
 
-
+        N lines: more input corresponding to the cozy levels of cats with families (?)
+    
     output:
+        2 ints on the same line, 
+            1. max coziness sum possible
+            2. coziness of least cozy cat under least possible cozy combo
 
 
+
+
+    SAMPLE INPUT: 
+
+        4           <- num of cats 
+
+        1 2 3       <- ? 
+        4 5 6
+        7 8 9
+        1 2 3
+
+        0 3 -3 2    <- ?
+        10 0 -6 4
+        5 2 0 8
+        0 0 0 0
+
+    SAMPLE OUTPUT: 
+
+        31 3 <- sum, least possible level
 
 */
 
