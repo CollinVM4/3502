330f215fbec1d2a2758a6a78b0e65e029c578ee8
collin vanmeter <metervancollin@gmail.com>
2024-02-12 14:24:43 -0500

    starting assignment 3

--- assignments/OptimalCozyLevel/optimalCozyLevel.c
+++ assignments/OptimalCozyLevel/optimalCozyLevel.c
@@ -7,6 +7,48 @@
 /*
     Assignment Outline
 
+    note: MUST USE RECURSION
+
+
+    no structs, only recursive function and main, use global memory to hold arrays and answers, 
+    account for negative answers, base case: make sure each hose has a cat assigned
+
+
+    input:
+
+
+    output:
+
 
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// function prototype
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
