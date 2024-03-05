2500e1fb85d979e7c8ad61930c8278a359089fea
collin vanmeter <metervancollin@gmail.com>
2024-03-04 20:49:31 -0500

    tree notes

--- classExamples/sorting/mergeSort.c
+++ classExamples/sorting/mergeSort.c
@@ -7,6 +7,17 @@
         * sort (n/2)
         * merge 
     
+        time complexity
+            M(n) = 2M(n/2)  (add here)
+
+            add these to the end for...
+            best - N/2 (one side empty)
+            worst - n-1
+
+            however can just say its N (basically the same)
+
+            M(n) = 2M(n/2) + n
+
 
 */
 
