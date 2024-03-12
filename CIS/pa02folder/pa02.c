07bf5feb6f1e5c48da98cc8efdbe10db6709d1da
collin vanmeter <metervancollin@gmail.com>
2024-03-12 16:12:44 -0400

    getting ready for pa02

---
+++ CIS/pa02folder/pa02.c
@@ -0,0 +1,45 @@
+/*============================================================================
+| Assignment: pa02 - XXXXXXXXXXXXXXXXXXX   
+|
+| Author: Collin Van Meter
+| Language: c
+|
+| To Compile: gcc -o pa02 pa01.c
+| To Execute: c -> ./pa02 kX.txt pX.txt
+| where kX.txt is the keytext file
+| and pX.txt is plaintext file
+| Note:
+| All input files are simple 8 bit ASCII input
+| All execute commands above have been tested on Eustis
+|
+| Class: CIS3360 - Security in Computing - Fall 2023
+| Instructor: McAlpin
+| Due Date: XXXXXXXXXXXXXXXXXXXX
++===========================================================================*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
+
+
+/*=============================================================================
+| I [Collin Van Meter] ([co149144]) affirm that this program is
+| entirely my own work and that I have neither developed my code together with
+| any another person, nor copied any code from any other person, nor permitted
+| my code to be copied or otherwise used by any other person, nor have I
+| copied, modified, or otherwise used programs created by others. I acknowledge
+| that any violation of the above terms will be treated as academic dishonesty.
++=============================================================================*/
